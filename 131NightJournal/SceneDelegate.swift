82eed4d136e3cf42b0298577512b43a8592602f7
Roman <romazaniosdev@gmail.com>
2026-05-03 10:55:29 +0300

    add view

--- 131NightJournal/SceneDelegate.swift
+++ 131NightJournal/SceneDelegate.swift
@@ -11,15 +11,16 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private var appRouter: AppRouter?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else {return}
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIHostingController(rootView: ContentView())
+        appRouter = AppRouter()
+        window?.rootViewController = appRouter?.initialViewController()
         window?.makeKeyAndVisible()
     }
-
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
